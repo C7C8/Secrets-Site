19997a27a097d4e8831f5363153d04ef61a85612
Sourec <tothemyers@gmail.com>
2018-10-02 19:31:39 -0400

    Start on main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'secrets-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'secrets';
+export class AppComponent implements OnInit {
+
+  constructor(private dialog: MatDialog ) { }
+
+  ngOnInit(): void {
+  }
+
+  sha1(): void {
+    this.dialog.open(HComponent);
+  }
 }
+
+@Component({
+  selector: 'secrets-h',
+  template: '<code>43fbcb37b4dc73c071b05fd5d66c6fc1302b8098</code>'
+})
+export class HComponent { }
